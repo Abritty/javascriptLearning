967b236543994d8ab7ebfde629948b75d003aeec
Shafia Afrin <shafia.afrincseruet15@gmail.com>
2025-03-17 21:28:55 +0100

    Create pyramid2.js

---
+++ pyramid2.js
@@ -0,0 +1,13 @@
+const character = "#";
+const count = 8;
+const rows = [];
+
+function padRow(rowNumber, rowCount) {
+  return (
+    " ".repeat(rowCount - rowNumber) +
+    character.repeat(2 * rowNumber - 1) +
+    " ".repeat(rowCount - rowNumber)
+  );
+}
+
+padRow(2, 5);
