7e2345baeaf398a110c00a0efbebdc01201f78a9
Shafia Afrin <shafia.afrincseruet15@gmail.com>
2025-08-14 02:05:51 +0200

    Update 2725.interval_cancellation.js

--- 2725.interval_cancellation.js
+++ 2725.interval_cancellation.js
@@ -6,17 +6,44 @@
  */
 
 var cancellable = function (fn, args, t) {
-  fn(...args);
+  fn(...args); // Call the function immediately
   const timer = setInterval(() => {
-    fn(...args);
+    fn(...args); // Call the function at intervals
   }, t);
-  let cancelFn = () => clearInterval(timer);
+  let cancelFn = () => clearInterval(timer); // Function to cancel the interval
   return cancelFn;
 };
 
-function exampleFunction(a) {
-  console.log(a * 2);
-}
+//test
+const result = [];
+const fn = (x) => x * 2;
+const args = [4],
+  t = 35,
+  cancelTimeMs = 190;
 
-const cancel = cancellable(exampleFunction, [4], 35);
-setTimeout(cancel, 190);
+const start = performance.now();
+
+const log = (...argsArr) => {
+  const diff = Math.floor(performance.now() - start);
+  result.push({ time: diff, returned: fn(...argsArr) });
+};
+
+const cancel = cancellable(log, args, t);
+
+setTimeout(cancel, cancelTimeMs);
+setTimeout(
+  () => {
+    console.log(result);
+  },
+  cancelTimeMs + t + 15,
+);
+
+//result
+// [
+//   { time: 0, returned: 8 },
+//   { time: 36, returned: 8 },
+//   { time: 72, returned: 8 },
+//   { time: 108, returned: 8 },
+//   { time: 144, returned: 8 },
+//   { time: 180, returned: 8 },
+// ];
