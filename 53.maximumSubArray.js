bf5720380ccbfab389df939cd321e838334db66c
Shafia Afrin <shafia.afrincseruet15@gmail.com>
2025-09-29 19:05:26 +0200

    Create 53.maximumSubArray.js

---
+++ 53.maximumSubArray.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maxSubArray = function(nums) {
+    let currentSum = nums[0];
+    let maxSum = nums[0];
+
+    for (let i = 1; i < nums.length; i++) {
+        currentSum = Math.max(nums[i], currentSum + nums[i]);
+        maxSum = Math.max(maxSum, currentSum);
+    }
+
+    return maxSum;
+};
+
+// Example usage:
+console.log(maxSubArray([-2,1,-3,4,-1,2,1,-5,4])); // Output: 6
